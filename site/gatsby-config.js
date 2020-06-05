7e71516ab9a0771dde7eb41a6f2634cb577d3c80
Coaco <naseeihility@163.com>
2020-06-05 23:03:38 +0800

    use pacakge

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `New Blog`,
+    title: `BLOG`,
     subTitle: [`To Be A Hero Or`, `Be Water My Friend`],
-    keywords: ["theme-demo", "blog"],
-    description: `This is a new blog.`,
-    author: `@authorName`,
-    siteUrl: "https://siteUrl",
+    keywords: ["Front-End", "Python", "blog"],
+    description: `This is a blog.`,
+    author: `Kevin`,
+    siteUrl: "https://www.gaococ.com",
     gitTalk: {
       id: "75ea2191c8da60f43dca",
       secret: "ab7f0489b457f11b193d94d752a01a591c73fcdb",
