6f3a5980c21a278703520ec2d113e9f0c0f9a57a
Coaco <naseeihility@163.com>
2020-06-06 15:23:50 +0800

    done

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,6 @@ module.exports = {
     description: `This is a blog.`,
     author: `Kevin`,
     siteUrl: "https://www.gaococ.com",
-    gitTalk: {
-      id: "75ea2191c8da60f43dca",
-      secret: "ab7f0489b457f11b193d94d752a01a591c73fcdb",
-      repo: "comments",
-      owner: "naseeihity",
-    },
   },
   plugins: [
     {
